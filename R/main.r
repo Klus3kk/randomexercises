81dc51bb9f8ddf0ce1febf4a7f408aaf879847f0
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-08-07 19:17:12 +0200

    R

---
+++ R/main.r
@@ -0,0 +1,37 @@
+# Comment
+print("Hello world!")
+test <- 5 + 5
+# Assign the same value
+var1 <- var2 <- var3 <- 6
+
+for (x in 1:10) {
+    print(x)
+}
+
+# Join two elements together
+paste("Here is the test: ", test)
+
+# Types of variables
+# Numeric
+x1 <- 10.5
+# Integer
+x2 <- 1000L
+# Complex
+x3 <- 9i + 3
+# Character
+x4 <- "Text"
+# Boolean
+x5 <- TRUE
+
+# Print the class name
+class(x)
+
+# Convertion: .numeric(), .integer(), .complex()
+
+# Mathematic functions
+min(5,2,1)
+max(5,67,1)
+sqrt(16)
+abs(-5)
+ceiling(1.4)
+floor(1.4)
