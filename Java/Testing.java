f5507960b446ee19d0a3c01e8ab6ea218ffb2c9f
Klus3kk <lukasz.bielaszewski@onet.pl>
2025-01-10 08:46:15 +0100

    Java testing.

---
+++ Java/Testing.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner; // input, reading a file
+import java.time.LocalDate; // date
+import java.time.LocalTime; // time
+import java.util.ArrayList; // arraylist
+import java.util.HashMap; //  hashmap
+import java.util.HashSet; // set
+import java.io.File; // files
+import java.io.FileWriter; // writing to files
+import java.io.IOException; // exceptions for files
+
+public class Main {
+    public static void main(String[] args) { 
+        System.out.println("Hello world!");
+        System.out.println(Math.sqrt(64));
+    }
+}
